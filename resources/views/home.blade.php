a5309460104a4d46d9dbe73df8d792e26a3ad2dd
putri.amelia <bcmputriamelia@gmail.com>
2025-02-17 11:14:35 +0700

    update testimoni

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -160,58 +160,58 @@
                 <section class="relative isolate bg-white lg:px-6 lg:py-24 px-4 py-14">
                     <div class="flex justify-center">
                         <div class="flex flex-col">
-                            <img class="mx-auto size-16 lg:size-40 rounded-full" src="{{ Storage::url('testimonial-photo/jamsyar.jpg') }}" alt="">
-                            <figcaption class="mt-2 lg:text-lg sm:text-sm text-center text-gray-500 dark:text-gray-400">Jamkrindo Syariah</figcaption>
+                            <img class="mx-auto size-16 lg:size-40 rounded-full" src="{{ Storage::url('client-logo/logo-bankkalsel.png') }}" alt="">
+                            <figcaption class="mt-2 lg:text-lg sm:text-sm text-center text-gray-500 dark:text-gray-400">Gusti M. Ready</figcaption>
                         </div>
-                        <a href="#" class="flex flex-col items-center bg-white rounded-lg md:flex-row md:max-w-4xl hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 ">
+                        <div class="flex flex-col items-center bg-white rounded-lg md:flex-row md:max-w-4xl hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 ">
 
                             <div class="flex flex-col justify-between px-4 lg:p-4 leading-normal">
-                                <h5 class="mb-2 text-xl lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">PT Jamkrindo Syariah</h5>
-                                <p class="mb-3 text-xs lg:text-xl text-gray-700 dark:text-gray-400">“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis."</p>
-                                <p class="mb-3 hidden lg:block lg:font-normal text-gray-700 dark:text-gray-400 text-right">Kepala Divisi Teknologi Informasi</p>
+                                <h5 class="mb-2 text-xl lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Bank Kalsel</h5>
+                                <p class="mb-3 text-xs lg:text-xl text-gray-700 dark:text-gray-400">“Baru kali pertama saya mengikuti training Sistem Manajemen Keselamatan & Kesehatan Kerja (SMK3) menggunakan Jasa Konsultan BCM Indonesia. Materinya yang disampaikan sangat bagus, banyak sharing dan studi kasus serta pengajarnya sangat ramah dan expert di bidangnya."</p>
+                                <p class="mb-3 hidden lg:block lg:font-normal text-gray-700 dark:text-gray-400 text-right">Bagian Umum Bank Kalsel</p>
                             </div>
-                        </a>
+                        </div>
                     </div>
                 </section>
             </div>
 
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
+            {{-- <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <section class="relative isolate bg-white lg:px-6 lg:py-24 px-4 py-14">
                     <div class="flex justify-center">
                         <div class="flex flex-col">
                             <img class="mx-auto size-16 lg:size-40 rounded-full" src="{{ Storage::url('testimonial-photo/jamkrindo.jpg') }}" alt="">
-                            <figcaption class="mt-2 lg:text-lg sm:text-sm text-center text-gray-500 dark:text-gray-400">Jamkrindo</figcaption>
-                        </div>
-                        <a href="#" class="flex flex-col items-center bg-white rounded-lg md:flex-row md:max-w-4xl hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 ">
-
-                            <div class="flex flex-col justify-between px-4 lg:p-4 leading-normal">
-                                <h5 class="mb-2 text-xl lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">PT Jamkrindo</h5>
-                                <p class="mb-3 text-xs lg:text-xl text-gray-700 dark:text-gray-400">“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis."</p>
-                                <p class="mb-3 hidden lg:block lg:font-normal text-gray-700 dark:text-gray-400 text-right">Kepala Divisi Pengembangan</p>
-                            </div>
-                        </a>
-                    </div>
-                </section>
-            </div>
-
+            <figcaption class="mt-2 lg:text-lg sm:text-sm text-center text-gray-500 dark:text-gray-400">Jamkrindo</figcaption>
         </div>
-        <!-- Slider controls -->
-        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 dark:bg-gray-800/30 group-hover:bg-black/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
-                </svg>
-                <span class="sr-only">Previous</span>
-            </span>
-        </button>
-        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 dark:bg-gray-800/30 group-hover:bg-black/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
-                </svg>
-                <span class="sr-only">Next</span>
-            </span>
-        </button>
+        <a href="#" class="flex flex-col items-center bg-white rounded-lg md:flex-row md:max-w-4xl hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-700 ">
+
+            <div class="flex flex-col justify-between px-4 lg:p-4 leading-normal">
+                <h5 class="mb-2 text-xl lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white">PT Jamkrindo</h5>
+                <p class="mb-3 text-xs lg:text-xl text-gray-700 dark:text-gray-400">“Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo expedita voluptas culpa sapiente alias molestiae. Numquam corrupti in laborum sed rerum et corporis."</p>
+                <p class="mb-3 hidden lg:block lg:font-normal text-gray-700 dark:text-gray-400 text-right">Kepala Divisi Pengembangan</p>
+            </div>
+        </a>
+    </div>
+    </section>
+    </div> --}}
+
+    </div>
+    <!-- Slider controls -->
+    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 dark:bg-gray-800/30 group-hover:bg-black/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+            </svg>
+            <span class="sr-only">Previous</span>
+        </span>
+    </button>
+    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 dark:bg-gray-800/30 group-hover:bg-black/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+            </svg>
+            <span class="sr-only">Next</span>
+        </span>
+    </button>
     </div>
     <!-- end testimoni -->
 
